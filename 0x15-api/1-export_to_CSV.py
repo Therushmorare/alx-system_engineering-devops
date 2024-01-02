9b97f8ce8fbc36e6ff2e0c50764771a7b2aefcb4
therushmorare <Tshego.tt.morare@gmail.com>
2024-01-02 07:12:00 -0800

    json rest api

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+task 1
+"""
+
+import csv
+import requests
+import sys
+
+base_url = 'https://jsonplaceholder.typicode.com/'
+
+
+def do_request():
+    '''Performs request'''
+
+    if not len(sys.argv):
+        return print('USAGE:', __file__, '<employee id>')
+    employee_id = sys.argv[1]
+    try:
+        _employee_id = int(sys.argv[1])
+    except ValueError:
+        return print('Employee id must be an integer')
+
+    response = requests.get(base_url + 'users/' + employee_id)
+    if response.status_code == 404:
+        return print('User id not found')
+    elif response.status_code != 200:
+        return print('Error: status_code:', response.status_code)
+    user = response.json()
+
+    response = requests.get(base_url + 'todos/')
+    if response.status_code != 200:
+        return print('Error: status_code:', response.status_code)
+    todos = response.json()
+    user_todos = [todo for todo in todos
+                  if todo.get('userId') == user.get('id')]
+    completed = [todo for todo in user_todos if todo.get('completed')]
+
+    with open(employee_id + '.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile, lineterminator='\n',
+                            quoting=csv.QUOTE_ALL)
+        [writer.writerow(['{}'.format(field) for field in
+                          (todo.get('userId'), user.get('username'),
+                           todo.get('completed'), todo.get('title'))])
+         for todo in user_todos]
+
+
+if __name__ == '__main__':
+    do_request()
