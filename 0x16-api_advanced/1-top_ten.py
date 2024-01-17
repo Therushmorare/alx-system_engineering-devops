3819e88853ead47366fe54e8c0b5e89a681afdd4
therushmorare <Tshego.tt.morare@gmail.com>
2024-01-16 16:13:18 -0800

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-
 """
 Task 1
 """
-
 import requests
 
+
 def top_ten(subreddit):
-        """ get top 10 posts """
+    posts = ""
+    if (type(subreddit) != str):
+        posts = "None\n"
+    else:
         api_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-        headers = {'user-agent': 'safari:holberton/0.1.0'}
+        headers = {'user-agent': 'safari:alx/0.1.0'}
         response = requests.get(api_url, headers=headers)
-
-        posts = ""
-        if (type(subreddit) != str):
-            posts = "None\n"
-
         if response.status_code != 200:
             posts = "None\n"
         else:
@@ -23,5 +20,4 @@ def top_ten(subreddit):
             for i in range(10):
                 post_title = posts_json[i].get("data").get("title")
                 posts += "{}\n".format(post_title)
-
-        return (print(posts, end=""))
+    print(posts, end="")
