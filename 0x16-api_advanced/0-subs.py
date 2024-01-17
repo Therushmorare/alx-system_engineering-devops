c298c72a0feb6864eaebb78effaa20c637c3822b
therushmorare <Tshego.tt.morare@gmail.com>
2024-01-16 16:01:42 -0800

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,12 +7,13 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ Init """
-    url_api = ("https://www.reddit.com/r/{}/about.json".format(subreddit))
-    headers = {'user-agent': 'safari:holberton/0.1.0'}
-    response = requests.get(url_api, headers=headers)
 
     if (type(subreddit) !=  str):
         return(0)
+
+    url_api = ("https://www.reddit.com/r/{}/about.json".format(subreddit))
+    headers = {'user-agent': 'safari:alx/0.1.0'}
+    response = requests.get(url_api, headers=headers)
 
     if response.status_code != 200:
         return (0)
